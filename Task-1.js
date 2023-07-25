0677d85fd70afcbd3f334fb3b1b9e626e3b1c0ae
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-07-25 13:10:28 +0600

    add task1

---
+++ Task-1.js
@@ -0,0 +1,13 @@
+// Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+// Example Input: "hello world" Example Output: "dlrow olleh"
+
+function reverseString(str) {
+  let afterReverse = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    afterReverse += str[i];
+  }
+  return afterReverse;
+}
+const result = reverseString("hello world");
+console.log(result); // dlrow olleh
