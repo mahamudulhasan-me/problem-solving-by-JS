b77ac122983e1551b7af999776cc5c1843e0e16c
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-07-25 15:25:32 +0600

    task 5 complete

--- Task-5.js
+++ Task-5.js
@@ -1,6 +1,6 @@
 // Task 5: Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
 
-const calculator = (num1, num2, operator) => {
+const simpleCalculator = (num1, num2, operator) => {
   switch (operator) {
     case "+":
       return num1 + num2;
@@ -15,4 +15,8 @@ const calculator = (num1, num2, operator) => {
   }
 };
 
-console.log(calculator(3, 6, "/"));
+const num1 = 5;
+const num2 = 3;
+const operator = "+";
+const result = simpleCalculator(num1, num2, operator);
+console.log(result); // Output: 8
