b209565d6587a20e2cd2d328e96af26af6778768
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-07-25 14:51:18 +0600

    add task 3

---
+++ Task-3.js
@@ -0,0 +1,24 @@
+// Task 3: Write a JavaScript program to find the most frequent element in an array and return it.
+
+// Example Input: [3, 5, 2, 5, 3, 3, 1, 4, 5, 2, 2, 8, 2, 2, 2, 2]
+
+const mostFrequentElement = (arr) => {
+  let obj = {};
+  for (const element of arr) {
+    obj[element] ? obj[element]++ : (obj[element] = 1);
+  }
+
+  let mostFrequent;
+  let maxCount = 0;
+
+  for (const num in obj) {
+    if (obj[num] > maxCount) {
+      maxCount = obj[num];
+      mostFrequent = Number(num);
+    }
+  }
+  return mostFrequent;
+};
+const arr = [3, 3, 3, 3, 3, 3, 3, 5, 5, 8, 7, 9, 4];
+const result = mostFrequentElement(arr);
+console.log(result);
